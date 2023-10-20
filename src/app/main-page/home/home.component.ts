bb46d52efc3603360c1723f33f987da8a82bc70e
Bùi Đức Hải <haccongtu1221@gmail.com>
2023-10-20 21:46:35 +0700

    update

--- src/app/main-page/home/home.component.ts
+++ src/app/main-page/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js'
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('tw') typewriteElement: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngAfterViewInit() {
+    const target = this.typewriteElement.nativeElement
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: 'black'
+    })
+
+    writer
+      .type("I'm a Developer")
+      .rest(300)
+      .changeOps({ deleteSpeed: 80 })
+      .remove(9)
+      .clear()
+      .start()
+  }
 }
