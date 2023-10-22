95df2cacef757e2c22c5e9f1d5c3f8d06c47e43e
Bùi Đức Hải <haccongtu1221@gmail.com>
2023-10-22 18:41:15 +0700

    update magic_mouse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { asyncScheduler, fromEvent, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ResumeCV';
+
+
+  @ViewChild('top') top!: ElementRef;
+  @ViewChild('bottom') bottom!: ElementRef;
+  subscription: any;
+  constructor() { }
+  ngOnInit() {
+    this.subscription = fromEvent(document, 'mousemove').pipe(throttleTime(50, asyncScheduler, { leading: true, trailing: true })).subscribe((e: any) => {
+      const coordinateX = e.clientX;
+      const coordinateY = e.clientY;
+      this.top.nativeElement.style.transform =
+        `translate(${coordinateX}px, ${coordinateY}px)`;
+      this.bottom.nativeElement.style.transform =
+        `translate(${coordinateX}px, ${coordinateY}px)`;
+    })
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 }
